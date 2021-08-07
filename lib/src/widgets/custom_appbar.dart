c680281a559adbaef12aff57f0f3072658510bba
Luis Rivera <lrivera@bevertec.com>
2021-08-07 02:40:19 -0500

    autofocus search

--- lib/src/widgets/custom_appbar.dart
+++ lib/src/widgets/custom_appbar.dart
@@ -2,10 +2,7 @@ import 'package:buscape/src/pages/product_desc_page.dart';
 import 'package:buscape/src/pages/product_search.dart';
 import 'package:flutter/material.dart';
 
-
-
 class CustomAppBar extends StatelessWidget {
-  
   final String texto;
   final bool readonly;
 
@@ -13,40 +10,51 @@ class CustomAppBar extends StatelessWidget {
     required this.texto,
     required this.readonly,
   });
-  void pushSearchScreen(BuildContext context){
-    Navigator.push(context,  MaterialPageRoute(builder: (BuildContext context) => ProductSearch() ) );
+  void pushSearchScreen(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) => ProductSearch()));
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: true,
       bottom: false,
       child: Padding(
-        padding: EdgeInsets.symmetric( horizontal: 30 ),
+        padding: EdgeInsets.symmetric(horizontal: 30),
         child: Container(
           margin: EdgeInsets.only(top: 30),
           width: double.infinity,
           child: Row(
             children: <Widget>[
-              Expanded(child: readonly ? 
-                GestureDetector(
-                  onTap: (){
-                    pushSearchScreen(context);
-                  },
-                  child: Text(this.texto.length == 0 ? 'Buscar...' : this.texto,
-                    style: TextStyle( fontSize: 30, color: Colors.grey, fontWeight: FontWeight.w700),),
-                ):
-                TextField( 
-                  decoration: InputDecoration(
-                    hintText: 'Buscar...',
-                  ),
-                  style: TextStyle( fontSize: 30, fontWeight: FontWeight.w700 ),
-                )
-              ),
-              GestureDetector(child: Icon( Icons.search, size: 30 ), onTap: (){
-                pushSearchScreen(context);
-              },) 
-
+              Expanded(
+                  child: readonly
+                      ? GestureDetector(
+                          onTap: () {
+                            pushSearchScreen(context);
+                          },
+                          child: Text(
+                            this.texto.length == 0 ? 'Buscar...' : this.texto,
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        )
+                      : TextField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            hintText: 'Buscar...',
+                          ),
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w700),
+                        )),
+              GestureDetector(
+                child: Icon(Icons.search, size: 30),
+                onTap: () {
+                  pushSearchScreen(context);
+                },
+              )
             ],
           ),
         ),
