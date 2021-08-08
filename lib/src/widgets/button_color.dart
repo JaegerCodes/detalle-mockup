7951b8045d8e72ca4e3637e54cd3580736d4f4ad
kperez <kperez>
2021-08-07 19:03:15 -0500

    carrusel

--- lib/src/widgets/button_color.dart
+++ lib/src/widgets/button_color.dart
@@ -3,7 +3,7 @@ import 'package:buscape/src/models/product_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ButtonColor extends StatelessWidget {
+class ButtonColor extends StatefulWidget {
   final Color color;
   final int index;
   final String urlImagen;
@@ -11,22 +11,26 @@ class ButtonColor extends StatelessWidget {
   const ButtonColor(this.color, this.index, this.urlImagen);
 
   @override
+  _ButtonColorState createState() => _ButtonColorState();
+}
+
+class _ButtonColorState extends State<ButtonColor> {
+  @override
   Widget build(BuildContext context) {
     return FadeInLeft(
-      delay: Duration(milliseconds: this.index * 100),
+      delay: Duration(milliseconds: this.widget.index * 100),
       duration: Duration(milliseconds: 300),
       child: GestureDetector(
         onTap: () {
           final productModel = Provider.of<ProductModel>(context, listen: false);
-          productModel.assetImage = this.urlImagen;
-          productModel.color = this.color;
-          productModel.setPresentation();
+          productModel.assetImage = this.widget.urlImagen;
+          productModel.color = this.widget.color;
         },
         child: Container(
           width: 45,
           height: 45,
           decoration: BoxDecoration(
-              color: color,
+              color: widget.color,
               shape: BoxShape.circle,
               border: Border.all(color: Colors.grey)),
         ),
